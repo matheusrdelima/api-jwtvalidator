ab2d372d1d3a6960ac038fd8f8c5de73bbd3a9eb
Matheus <matheusrdelima@outlook.com>
2025-08-18 22:53:26 -0300

    feat: adicionado regra para validacao da claim Name

--- src/main/java/com/jwtvalidator/validator/NameValidator.java
+++ src/main/java/com/jwtvalidator/validator/NameValidator.java
@@ -1,16 +1,38 @@
 package com.jwtvalidator.validator;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Component;
 
 import com.jwtvalidator.model.Claims;
+import com.jwtvalidator.util.StringHasNumber;
 
 @Component
 public class NameValidator implements Validator {
 
     public static final String VALIDATOR_NAME = "NameValidator";
+    private static final int MAX_NAME_LENGTH = 256;
 
     @Override
     public Boolean validate(Claims claims) {
+        if (Objects.isNull(claims) || Objects.isNull(claims.getName())) {
+            System.out.println("Claim ou Name nulo");
+            return false;
+        }
+
+        String name = claims.getName();
+
+        if (name.length() > MAX_NAME_LENGTH) {
+            System.out.println("Name maior que o tamanho permitido de caracteres, quantidade atual: " + name.length()
+                    + " quantidade maxima: " + MAX_NAME_LENGTH);
+            return false;
+        }
+
+        if (StringHasNumber.check(name)) {
+            System.out.println("Name contém números: " + name);
+            return false;
+        }
+
         return true;
     }
 
