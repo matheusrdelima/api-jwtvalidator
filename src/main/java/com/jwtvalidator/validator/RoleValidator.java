60a765a486c48a6debd6f5fc752d69451768ccf6
Matheus <matheusrdelima@outlook.com>
2025-08-18 23:04:17 -0300

    feat: adicionado regra para validacao da claim Role

--- src/main/java/com/jwtvalidator/validator/RoleValidator.java
+++ src/main/java/com/jwtvalidator/validator/RoleValidator.java
@@ -1,5 +1,8 @@
 package com.jwtvalidator.validator;
 
+import java.util.Objects;
+import java.util.Set;
+
 import org.springframework.stereotype.Component;
 
 import com.jwtvalidator.model.Claims;
@@ -8,10 +11,18 @@ import com.jwtvalidator.model.Claims;
 public class RoleValidator implements Validator {
 
     public static final String VALIDATOR_NAME = "RoleValidator";
+    private static final Set<String> ROLES = Set.of("Admin", "Member", "External");
 
     @Override
     public Boolean validate(Claims claims) {
-        return true;
+        if (Objects.isNull(claims) || Objects.isNull(claims.getRole())) {
+            System.out.println("Claim ou Role nulo");
+            return false;
+        }
+
+        System.out.println("Role: " + claims.getRole());
+
+        return ROLES.contains(claims.getRole());
     }
 
     @Override
