b7d583c590e950b8c2cf6843f4fbafa4b479abf3
Matheus <matheusrdelima@outlook.com>
2025-08-19 22:27:12 -0300

    feat: adicionado validacoes basicas

--- src/main/java/com/jwtvalidator/model/JwtValidationRequest.java
+++ src/main/java/com/jwtvalidator/model/JwtValidationRequest.java
@@ -1,7 +1,12 @@
 package com.jwtvalidator.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class JwtValidationRequest {
-    
+
+    @NotNull(message = "JWT token não pode ser nulo")
+    @NotBlank(message = "JWT token não pode estar vazio")
     private String jwt;
 
     public JwtValidationRequest() {}
