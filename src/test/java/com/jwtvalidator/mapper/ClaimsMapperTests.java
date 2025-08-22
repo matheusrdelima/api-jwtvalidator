b88220796c3816cac1b3ef67a35c09cfba0a8625
Matheus Rodrigues de Lima <matheusrdelima@outlook.com>
2025-08-21 23:17:56 -0300

    tests: adicionado testes unitarios

---
+++ src/test/java/com/jwtvalidator/mapper/ClaimsMapperTests.java
@@ -0,0 +1,45 @@
+package com.jwtvalidator.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.jwtvalidator.model.Claims;
+
+@ExtendWith(MockitoExtension.class)
+public class ClaimsMapperTests {
+
+    @Test
+    public void claimsMapperTests() {
+        String name = "name";
+        String role = "Admin";
+        String seed = "2";
+
+        Map<String, String> claimsMap = Map.of(
+                "Name", name,
+                "Role", role,
+                "Seed", seed);
+
+        ClaimsMapper claimsMapper = new ClaimsMapper();
+        Claims claims = claimsMapper.toClaims(claimsMap);
+
+        assertEquals(claims.getName(), name);
+        assertEquals(claims.getRole(), role);
+        assertEquals(claims.getSeed(), seed);
+        assertEquals(claims.getAllClaims(), claimsMap);
+    }
+
+    @Test
+    public void claimsMapperMalformedJwtExceptionTests() {
+        ClaimsMapper claimsMapper = new ClaimsMapper();
+
+        assertThrows(com.jwtvalidator.exception.MalformedJwtException.class, () -> {
+            claimsMapper.toClaims(null);
+        });
+    }
+}
